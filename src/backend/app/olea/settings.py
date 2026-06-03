2a5c230558b2c3920b649d945cf2fca3469e8d12
Antonlopex <antonlopez2004@gmail.com>
2026-06-03 14:17:42 +0200

    cambio de envio de mail para railway

--- src/backend/app/olea/settings.py
+++ src/backend/app/olea/settings.py
@@ -165,8 +165,9 @@ USE_TZ = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
